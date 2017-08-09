ea6574733ea9e910e89dd0ed48a0b54af713096d
Denniskamau <denniskamau3@gmail.com>
2017-08-09 14:17:16 +0300

    refactored regester

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
-import {User} from '../../User'
+import {User} from '../../User';
+import {AbstactControl,formArray,FormBuilder,FormControl,FormGroup,validators} from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  providers:[UserService]
+  providers:[UserService] 
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
+  
   users:User[];
   username:string;
   email:string;
   password:string;
- constructor(private userService: UserService) { 
+ constructor(private userService: UserService,public fb:FormBuilder) { 
+
+    this.form = this.fb.group({
+      username:'',
+      email:'',
+      password:''
+    })
     this.userService.getUsers()
       .subscribe(users =>{
         this.users=users;
       })
     }
-
+    // Called in the event of a submit in the html to add a new user.
   addUser(event){
     event.preventDefault();
     var newUser = {
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import {Injectable}from '@angular/core';
 import {Http,Headers} from '@angular/http';
+//observable is an ES7 feature hence RXJS is used for manipulating data.
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -7,11 +8,12 @@ export class UserService{
     constructor(private http:Http){
         console.log('User Service Initialized..');
     }
+    //Getting users from the API
     getUsers(){
         return this.http.get('/api/users')
             .map(res => res.json());
     }
-    
+    //Adding new users to the API
     addUser(newuser){
         console.log(newuser);
         var headers=new Headers();
